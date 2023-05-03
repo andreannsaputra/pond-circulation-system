4cde61e82fb4e1378b4fb89795a4b6fe1779be28
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-05-03 14:06:24 +0700

    add ultrasonic code to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int trigPin = 11;
+int echoPin = 12;
+long duration, cm;
+
+void setup()
+{
+  // Serial Port begin
+  Serial.begin(9600);
+  // Define I/O
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read the signal from the sensor // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.  duration = pulseIn (echoPin, HIGH);
 }
