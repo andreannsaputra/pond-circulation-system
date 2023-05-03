0a276f8855e63728b8e0e66c36554e547f1e72c7
Muhammad Andrean Saputra <andreannproject@gmail.com>
2023-05-03 14:20:03 +0700

    add ultrasonic.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <Arduino.h>
-
-int trigPin = 11;
-int echoPin = 12;
-long duration, cm;
+#include <ultrasonic.h>
 
 void setup()
 {
@@ -15,14 +12,8 @@ void setup()
 
 void loop()
 {
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
 
-  // Read the signal from the sensor // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.  duration = pulseIn (echoPin, HIGH);
+  ultrasonic();
+
+  delay(250);
 }
---
+++ src/ultrasonic.h
@@ -0,0 +1,21 @@
+#include <Arduino.h>;
+
+void ultrasonic()
+{
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(5);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Read the signal from the sensor // Read the signal from the sensor: a HIGH pulse whose
+    // duration is the time (in microseconds) from the sending
+    // of the ping to the reception of its echo off of an object.  duration = pulseIn (echoPin, HIGH);
+    duration = pulseIn(echoPin, HIGH);
+
+    // Convert the time into a distance
+    cm = (duration / 2) / 29.1;
+
+    Serial.print(cm);
+}
